58bb91022eff9bd5d8306939d13b4036cfb39cf7
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-10-20 12:58:20 +0200

    finish chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -36,17 +36,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-          children: groupedTransactionValues
-              .map(
-                (data) => ChartBar(
-                    label: data['day'],
-                    spendingAmount: data['amount'],
-                    spendingPctOfTotal: totalSpending == 0.0
-                        ? 0.0
-                        : (data['amount'] as double) / totalSpending),
-              )
-              .toList()),
+      child: Container(padding: EdgeInsets.all(10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactionValues
+                .map(
+                  (data) => Flexible(
+                    fit: FlexFit.tight,
+                    child: ChartBar(
+                        label: data['day'],
+                        spendingAmount: data['amount'],
+                        spendingPctOfTotal: totalSpending == 0.0
+                            ? 0.0
+                            : (data['amount'] as double) / totalSpending),
+                  ),
+                )
+                .toList()),
+      ),
     );
   }
 }
