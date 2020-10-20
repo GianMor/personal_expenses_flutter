bee22eed568bdd02c7a48fe75347bfbb16d57352
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-10-20 11:05:13 +0200

    add image if list empty

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,40 +9,52 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.center,
         height: 300,
-        child: ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-                child: Row(children: <Widget>[
-              Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Theme.of(context).primaryColorLight, width: 2)),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '\$${transaction[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColorDark),
-                  )),
-              Column(
+        child: transaction.isEmpty
+            ? Column( 
                 children: <Widget>[
-                  Text(
-                    transaction[index].title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  Text(
-                    DateFormat()
-                        .add_yMMMMEEEEd()
-                        .format(transaction[index].date),
-                    style: TextStyle(fontSize: 14, color: Colors.grey),
-                  )
+                  Text('No transaction added yet',
+                      style: Theme.of(context).textTheme.headline6),
+                  Container(height: 200,margin: EdgeInsets.all(15),child: Image.asset('assets/images/waiting.png',fit: BoxFit.cover,))
                 ],
               )
-            ]));
-          },
-          itemCount: transaction.length,
-        ));
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                      child: Row(children: <Widget>[
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).primaryColorLight,
+                                width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$${transaction[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColorDark),
+                        )),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          transaction[index].title,
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text(
+                          DateFormat()
+                              .add_yMMMMEEEEd()
+                              .format(transaction[index].date),
+                          style: TextStyle(fontSize: 14, color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ]));
+                },
+                itemCount: transaction.length,
+              ));
   }
 }
